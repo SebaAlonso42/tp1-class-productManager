a7da2406d971691a733b0ef2bd0f3e86b84ce00d
SebaAlonso42 <sebaa.alonso42@gmail.com>
2023-04-17 20:14:37 -0300

    entrega tp1

--- productManager.js
+++ productManager.js
@@ -1,82 +1,76 @@
 class ProductManager {
     constructor() {
-        this.products = []; // Inicializa el array de productos vacío
+    this.products = [];
+    this.productId = 0;
     }
 
-    addProduct(title, description, price, thumbnail, code, stock) {
-        const newProduct = { title, description, price, thumbnail, code, stock };
-        this.products.push(newProduct); // Agrega un nuevo producto al array
+// Validar que todos los campos sean obligatorios
+    addProduct(product) {
+    if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock) {
+        console.error('Todos los campos son obligatorios');
+        return;
     }
 
-    removeProduct(code) {
-        const index = this.products.findIndex((product) => product.code === code);
-        if (index > -1) {
-          this.products.splice(index, 1); // Remueve un producto del array
-        }
+// Validar que no se repita el campo "code"
+    const existingProduct = this.products.find(p => p.code === product.code);
+    if (existingProduct) {
+        console.error(`Ya existe un producto con el código ${product.code}`);
+        return;
     }
 
-    getProductByCode(code) {
-        return this.products.find((product) => product.code === code); // Retorna un producto según su código
+// Agregar el producto con id autoincrementable
+    this.productId++;
+    const newProduct = { id: this.productId, ...product };
+    this.products.push(newProduct);
+    console.log(`Producto agregado correctamente con id ${this.productId}`);
     }
 
     getProducts() {
-        return this.products; // Retorna todos los productos
+    return this.products;
     }
+
+    getProductById(id) {
+    const product = this.products.find(p => p.id === id);
+    if (product) {
+        return product;
+    } else {
+        console.error(`Producto con id ${id} no encontrado`);
+            }
+        }
 }
 
-// objeto para almacenar todas las instancias de ProductManager
-const productManagers = {};
-
-// funcion para agregar una instancia a mi objeto
-function addProductManager(key, instance) {
-    productManagers[key] = instance;
-}
-
-// Creo una instancia de ProductManager para "Juego de Pesas"
-const pesasManager = new ProductManager();
-const pesasProduct = {
-    title: 'Juego de Pesas',
-    description: 'Un set de pesas para entrenamiento físico en casa.',
-    price: 2499.99,
-    thumbnail: 'ruta/imagen-pesas',
-    code: 'PROD001',
-    stock: 20
-};
-pesasManager.addProduct(pesasProduct.title, pesasProduct.description, pesasProduct.price, pesasProduct.thumbnail, pesasProduct.code, pesasProduct.stock);
-addProductManager(pesasProduct.code, pesasManager);
-
-// otra instancia de ProductManager para "Smartwatch"
-const smartwatchManager = new ProductManager();
-const smartwatchProduct = {
-    title: 'Smartwatch',
-    description: 'Un reloj inteligente con pantalla táctil y muchas funcionalidades.',
-    price: 1599.99,
-    thumbnail: 'ruta/imagen-smartwatch',
-    code: 'PROD002',
-    stock: 15
-};
-smartwatchManager.addProduct(smartwatchProduct.title, smartwatchProduct.description, smartwatchProduct.price, smartwatchProduct.thumbnail, smartwatchProduct.code, smartwatchProduct.stock);
-addProductManager(smartwatchProduct.code, smartwatchManager);
-
-// otra instancia de ProductManager para "Bicicleta"
-const productManager = new ProductManager();
 const biciProduct = {
-    title: 'Bicicleta',
-    description: 'Rodado 28, cuadro de aluminio y 21 velocidades.',
-    price: 1950.99,
-    thumbnail: 'ruta/imagen-bicicleta',
-    code: 'PROD003',
+    title: "Bicicleta",
+    description: "Una bicicleta de alta calidad",
+    price: 500,
+    thumbnail: "https://example.com/bici.jpg",
+    code: "BIC001",
     stock: 10
 };
-productManager.addProduct(biciProduct.title, biciProduct.description, biciProduct.price, biciProduct.thumbnail, biciProduct.code, biciProduct.stock);
-addProductManager(biciProduct.code, productManager);
 
-//Imprimo por Consola
+const pesasProduct = {
+    title: "Juego de Pesas",
+    description: "Un juego completo de pesas para entrenamiento",
+    price: 200,
+    thumbnail: "https://example.com/pesas.jpg",
+    code: "PES001",
+    stock: 5
+};
 
-console.log(productManagers['PROD001'].getProducts());
+const caminadoraProduct = {
+    title: "Caminadora",
+    description: "Una caminadora de alta resistencia para entrenamiento intensivo",
+    price: 1000,
+    thumbnail: "https://example.com/caminadora.jpg",
+    code: "CAM001",
+    stock: 3
+};
 
-console.log(productManagers['PROD002'].getProducts());
+const manager = new ProductManager();
+manager.addProduct(biciProduct);
+manager.addProduct(pesasProduct);
+manager.addProduct(caminadoraProduct);
 
-console.log(productManagers['PROD003'].getProducts());
+//Imprimir por consola todos los productos
 
-
+console.log(manager.getProducts());
